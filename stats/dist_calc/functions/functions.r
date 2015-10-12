0876b017b85484c94a9f16058f81fe89c0c3097e
aj2duncan <duncanaj2@gmail.com>
2015-10-12 15:16:02 +0100

    Adding distribution calculator.

---
+++ stats/dist_calc/functions/functions.r
@@ -0,0 +1,3 @@
+find_normal_step = function(sd) {
+  10 ^ round(log(7 * sd/100, 10))
+}
---
+++ stats/dist_calc/ui.R
@@ -0,0 +1,36 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  
+  headerPanel("Distribution Calculator"),
+  
+  sidebarPanel(
+    #radio button or dropdown?
+
+    selectInput(inputId = "dist",
+                label = "Distribution:",
+                choices = c("Normal" = "rnorm",
+                            "Binomial" = "rbinom"),
+                selected = "rnorm"),
+    br(),
+    uiOutput("mean"),
+    uiOutput("sd"),
+    uiOutput("n"),
+    uiOutput("p"),
+    br(),
+    br(),
+    h4("Model:"),
+    div(textOutput("model"), style = "text-indent:20px;font-size:125%;"),
+    br(),
+    uiOutput("tail"),
+    uiOutput("lower_bound"),
+    uiOutput("upper_bound"),
+    uiOutput("a"),
+    uiOutput("b")
+  ), #ending sidebarPanel
+  mainPanel(
+    div(p("This app allows you to calculate the probability of an event based on a normal distribution or a binomial distribution. You can change the type of test you wish to do by changing the model. The distribution values and the value being tested can be altered by changing the sliders.")),
+    plotOutput("plot"),
+    div(textOutput("area"), align = "center", style = "font-size:150%;")
+  ) #ending mainPanel
+))
